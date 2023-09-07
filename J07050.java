1bf54b98dde735842285180d92d9a28888ed5b6a
ChangeOfHeart <127370973+Codebutproblem@users.noreply.github.com>
2023-09-07 09:27:10 +0700

    Create J07050.java

---
+++ J07050.java
@@ -0,0 +1,56 @@
+
+import java.io.*;
+import java.math.BigInteger;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.*;
+class Hang implements Comparable<Hang>{
+    private String id,ten,nhom;
+    private double mua,ban;
+
+    public Hang(int i, String ten, String nhom, double mua, double ban) {
+        this.id = i + "";
+        if(this.id.length() < 2) this.id = "0"+this.id;
+        this.id = "MH" + this.id;
+        this.ten = ten;
+        this.nhom = nhom;
+        this.mua = mua;
+        this.ban = ban;
+    }
+    public double getLoiNhuan(){
+        return ban - mua;
+    }
+
+    @Override
+    public int compareTo(Hang o) {
+        if(this.getLoiNhuan() < o.getLoiNhuan()){
+            return 1;
+        }
+        else{
+            return -1;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + ten + " " + nhom + " " +String.format("%.2f", this.getLoiNhuan());
+    }
+    
+}
+public class Main {
+    public static void main(String[] args)throws IOException {
+        Scanner sc = new Scanner(new File("MATHANG.in"));
+        int n = sc.nextInt();
+        Hang[] arr = new Hang[n];
+        sc.nextLine();
+        for(int i = 0; i < n ;i++){
+            arr[i] = new Hang(i+1,sc.nextLine(),sc.nextLine(),Double.parseDouble(sc.nextLine()),Double.parseDouble(sc.nextLine()));
+        }
+        Arrays.sort(arr);
+        for(Hang x : arr){
+            System.out.println(x);
+        }
+    }
+}
