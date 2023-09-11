a4da490509f88bfb7f03e380804ee5c5e9bd4c24
ChangeOfHeart <127370973+Codebutproblem@users.noreply.github.com>
2023-09-11 10:02:48 +0700

    Create J01002.java

---
+++ J01002.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0){
+            int n = sc.nextInt();
+            System.out.println(1l*n*(n+1)/2);
+        }
+    }
+    
+}
