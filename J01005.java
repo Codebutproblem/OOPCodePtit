a8f7b8d9e0361976127063b200b6a5174049d51b
ChangeOfHeart <127370973+Codebutproblem@users.noreply.github.com>
2023-09-11 10:16:27 +0700

    Update J01005.java

--- J01005.java
+++ J01005.java
@@ -3,7 +3,7 @@ import java.io.File;
 import java.util.*;
 
 
-public class Main {
+public class J01005 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
