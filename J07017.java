8ea05767d6b88b109c0a95c4da18c3f9e11cda3a
__Dark_____and_____Light__ <127370973+Codebutproblem@users.noreply.github.com>
2023-10-01 09:47:02 +0700

    Create J07017.java

---
+++ J07017.java
@@ -0,0 +1,64 @@
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+class Pair<K,V>{
+    private K key;
+    private V value;
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+    public static boolean nt(int n){
+        for(int i = 2; i <= Math.sqrt(n); i++){
+            if(n%i == 0) return false;
+        }
+        return n > 1;
+    }
+    public boolean isPrime(){
+        return nt((Integer)key) && nt((Integer)value);
+    }
+
+    @Override
+    public String toString() {
+        return key + " " + value;
+    }
+    
+}
+public class Main {  
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("DATA.in"));
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            boolean check = false;
+            for(int i = 2; i <= 2*Math.sqrt(n); i++){
+                Pair<Integer, Integer> p = new Pair<>(i, n-i);
+                if(p.isPrime()){
+                    System.out.println(p);
+                    check = true;
+                    break; 
+                }
+            }
+            if(!check) System.out.println(-1);
+        }
+    }
+    public static void main3449849(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("DATA.in"));
+        int t = sc.nextInt();
+        while(t-->0){
+            int n = sc.nextInt();
+            boolean check = false;
+            for(int i = 2; i <= 2*Math.sqrt(n); i++){
+                Pair<Integer, Integer> p = new Pair<>(i, n-i);
+                if(p.isPrime()){
+                    System.out.println(p);
+                    check = true;
+                    break; 
+                }
+            }
+            if(!check) System.out.println(-1);
+        }
+    }
+}
+
