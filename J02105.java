bdc8e26465b1bf81c210883c5f207bc231aeaf0a
__Dark_____and_____Light__ <127370973+Codebutproblem@users.noreply.github.com>
2023-10-01 09:11:34 +0700

    Create J02105.java

---
+++ J02105.java
@@ -0,0 +1,38 @@
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+
+
+public class J02105 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = 1;
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int[][] a = new int[n+1][n+1];
+            for(int i = 1; i <= n; i++){
+                for(int j = 1; j <= n; j++){
+                    a[i][j] = sc.nextInt();
+                }
+            }
+            for(int i = 1; i <= n; i++){
+                System.out.print("List("+i+") = ");
+                for(int j = 1; j <= n; j++){
+                    if(a[i][j] == 1){
+                        System.out.print(j +" ");
+                    }
+                }
+                System.out.println("");
+            }
+        }
+    }
+    
+}
