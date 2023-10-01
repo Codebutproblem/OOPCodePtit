c87f54a014ea8a056e720a668fead956c8a1fe6a
__Dark_____and_____Light__ <127370973+Codebutproblem@users.noreply.github.com>
2023-10-01 22:38:48 +0700

    Create J05022.java

---
+++ J05022.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+class SV implements Comparable<SV>{
+    private String id,ten,lop,email;
+
+    public SV(String id, String ten, String lop, String email) {
+        this.id = id;
+        this.ten = ten;
+        this.lop = lop;
+        this.email = email;
+    }
+
+    public String getLop() {
+        return lop;
+    }
+    
+    @Override
+    public int compareTo(SV o) {
+        if(!this.lop.equals(o.lop))return this.lop.compareTo(o.lop);
+        return this.id.compareTo(o.id);
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + ten + " " + lop + " " + email;
+    }
+    
+}
+public class J05022{
+    public static void main(String[] args) throws ParseException{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        SV[] arr = new SV[n];
+        sc.nextLine();
+        for(int i = 0; i < n ;i++){
+            arr[i] = new SV(sc.nextLine(),sc.nextLine(),sc.nextLine(),sc.nextLine());
+        }
+        int t = sc.nextInt();
+        while(t-- > 0){
+            String lop = sc.next();
+            System.out.println("DANH SACH SINH VIEN LOP " + lop+":");
+            for(SV x : arr){
+                if(x.getLop().equals(lop)){
+                    System.out.println(x);
+                }
+            }
+        }
+        
+    }
+}
