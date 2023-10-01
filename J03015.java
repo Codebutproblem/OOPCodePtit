f7551de48b5c4d58eeca878d4b62561d0e8bc349
__Dark_____and_____Light__ <127370973+Codebutproblem@users.noreply.github.com>
2023-10-01 09:17:54 +0700

    Create J03015.java

---
+++ J03015.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+
+public class J03015 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = 1;
+        while(t-- >0){
+            String x = sc.next(), y =sc.next();
+            for(int i = 0; i < x.length(); i++){
+                if(x.charAt(i) != '0'){
+                    x = x.substring(i);
+                    break;
+                }
+            }
+            for(int i = 0; i < y.length(); i++){
+                if(y.charAt(i) != '0'){
+                    y = y.substring(i);
+                    break;
+                }
+            }
+            BigInteger a = new BigInteger(x);
+            BigInteger b = new BigInteger(y);
+            System.out.println(a.subtract(b));
+        }
+    }   
+    
+}
