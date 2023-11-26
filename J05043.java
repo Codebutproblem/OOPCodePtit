41c4a7c988ac53dd540c1baad1b54d8e016a22b2
minh_vu0201 <127370973+Codebutproblem@users.noreply.github.com>
2023-11-26 15:08:42 +0700

    Create J05043.java

---
+++ J05043.java
@@ -0,0 +1,65 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+
+class NV{
+    private String id, name,cv;
+    private int lcb,nc;
+
+    public NV(int cnt,String name, String cv, int lcb, int nc) {
+        this.id = String.format("NV%02d", cnt);
+        this.name = name;
+        this.cv = cv;
+        this.lcb = lcb;
+        this.nc = nc;
+    }
+    public int getPhuCap(){
+        switch(cv){
+            case "GD":
+                return 500;
+            case "PGD":
+                return 400;
+            case "TP":
+                return 300;
+            case "KT":
+                return 250;
+            default:
+                return 100;
+        }
+    }
+    public int getLuongChinh(){
+        return lcb*nc;
+    }
+    public int getTamUng(){
+        if( 1f*(getPhuCap() + getLuongChinh())*2/3 < 25000 ){
+            return (int)Math.round((1f*(getPhuCap() + getLuongChinh())*2/3000))*1000;
+        }
+        return 25000;
+    }
+
+    @Override
+    public String toString() {
+        return id + " " + name + " " + getPhuCap() + " " + getLuongChinh() + " " + getTamUng() + " " + (getPhuCap()+getLuongChinh()-getTamUng());
+    }
+    
+}
+public class J05043 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        NV[]arr = new NV[n];
+        for(int i = 0; i < n; i++){
+            sc.nextLine();
+            arr[i] = new NV(i+1,sc.nextLine(),sc.nextLine(),sc.nextInt(),sc.nextInt());
+        }
+        for(NV x : arr){
+            System.out.println(x);
+        }
+    }
+}
